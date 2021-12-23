86692e5f4fb65ddfdf502871169230112a386d54
Rasheli-Jayalath <16.rasheli@gmail.com>
2021-12-23 09:23:01 +0530

    Updated : form-user-and-canal sub comp select dropdown

--- scripts/ajaxData2.php
+++ scripts/ajaxData2.php
@@ -2,27 +2,25 @@
 <?php 
 // Include the database config file 
 include_once '../Config/connection.php'; 
- 
+
 if(!empty($_POST["ca_id"])){ 
     $q = $_POST["ca_id"] ;
-    $sql = "SELECT * FROM wh_000_command_area WHERE ca_parent_id  = ".$_POST['ca_id']."  ";
-    $result = mysqli_query($connection, $sql) or die( mysqli_error($connection));
-    $num_of_raws = mysqli_num_rows($result);
-    if($num_of_raws>1 AND $q!=0 ){
-    
-        echo "    <div  class = \"form-group\" >";
-        echo "   Select the Sub component : &nbsp; ";
-        echo "   <select name= \"sub_comp_id\" class=\"form-select move-on-hover\">";
-        echo "   <option value=\"0\">None</option>";
-      
-        while($row = mysqli_fetch_array($result)){
-              echo " <option value=".$row['ca_code'].">".$row['ca_name']."</option>";
-                        }
-                                                  
-        echo "  </select>";
-        echo "  </div>";
+    if($q>0){
+        $query = "SELECT * FROM wh_000_command_area WHERE ca_parent_id  = ".$_POST['ca_id']."  ";
+        $result = $connection->query($query); 
+        
+        // Generate HTML of state options list 
+        if($result->num_rows > 0 && $q>0){ 
+            echo 'Select sub componant : &nbsp;' ;
+            echo '<select  name="" class="form-select move-on-hover" >';
+            
+            echo '<option value="">Select Sub component </option>'; 
+            while($row = $result->fetch_assoc()){  
+                echo " <option value=".$row['ca_code'].">".$row['ca_name']."</option>";
+            } 
+            echo '</select>' ;
+        }
     }
- 
 }
 
 
